edfebc587a4a5a413b49ca3932794e0d61c6d3d0
tt124640432 <906160281@qq.com>
2017-02-04 15:31:03 +0800

    first

---
+++ js/myswiper.js
@@ -0,0 +1,10 @@
+new Swiper(".swiper-o", {
+    /* 圆点*/
+    pagination: ".homebox",
+    /*滚动方向*/
+    direction: "horizontal",
+    /*圆点可点击*/
+    paginationClickable: true,
+    /*是否手指滑动效果*/
+    touchRatio: 1
+});
